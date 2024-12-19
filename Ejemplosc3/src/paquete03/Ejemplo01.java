0e67367e656c23c140598e42cb0d4e7e90c622bf
Joeldo04 <joeladominguez04@gmail.com>
2024-12-19 11:42:51 -0500

    S3

--- Ejemplosc3/src/paquete03/Ejemplo01.java
+++ Ejemplosc3/src/paquete03/Ejemplo01.java
@@ -10,43 +10,59 @@ package paquete03;
  * @author reroes
  */
 public class Ejemplo01 {
-    
+
     public static void main(String[] args) {
-        double[][] notas = {{9, 10, 7, 8}, 
-                            {10, 5, 7, 9}, 
-                            {7, 9.4, 10, 9}};
+        double[][] notas = {{9, 10, 7, 8},
+        {10, 5, 7, 9},
+        {7, 9.4, 10, 9}};
         String[] nombresEstudiante = {"Jose A", "Elena J", "Paúl L"};
         double[] sumaNotas = new double[3];
-        double[] sumaNotasCualitativas = new double[3];
+        String[] NotasCualitativas = new String[3];
         // 0 - 5.9 Bueno
         // 6 - 8.9 Muy Bueno
         // 9 - 10 Sobresaliente
-                    
+
         double suma;
+        double promedio;
         String reporte = "";
-        
+
         for (int i = 0; i < notas.length; i++) { // 0<3
             suma = 0; // 0
             for (int j = 0; j < notas[i].length; j++) { // 0<notas[0].length
-                                                        // 0<4 // 1<4 // 2<4 // 3<4 // 4<4
+                // 0<4 // 1<4 // 2<4 // 3<4 // 4<4
                 suma = suma + notas[i][j];
-                     // 0 + notas[0][0] // 0 + 9 // 9
-                     // 9 + notas[0][1] // 9 + 10 // 19
-                     // 19 + notas[0][2] // 19 + 7 // 26
-                     // 26 + notas[0][3] // 26 + 8 // 34
+                // 0 + notas[0][0] // 0 + 9 // 9
+                // 9 + notas[0][1] // 9 + 10 // 19
+                // 19 + notas[0][2] // 19 + 7 // 26
+                // 26 + notas[0][3] // 26 + 8 // 34
             }
-            sumaNotas[i] = suma; // 
+            sumaNotas[i] = suma;
+            promedio = suma / notas[i].length;
+            if (promedio >= 0 && promedio <= 5.9) {
+                NotasCualitativas[i] = "Bueno";
+            } else {
+                if (promedio >= 6 && promedio <= 8.9) {
+                    NotasCualitativas[i] = " Muy Bueno";
+                } else {
+                    if (promedio >= 9 && promedio <= 10) {
+                        NotasCualitativas[i] = " Sobresaliente";
+
+                    }  
+                    
+
+                }
+            }
         }
-        // Presentación de resultados
         for (int i = 0; i < nombresEstudiante.length; i++) {
-            reporte = String.format("%sEstudiante %s tiene una sumataria "
-                    + "de notas: %.3f\n",
-                    reporte,
-                    nombresEstudiante[i],
-                    sumaNotas[i]);
-        }
-        
-        System.out.printf("%s\n", reporte);
-        
+                        reporte = String.format("%sEstudiante %s tiene una sumataria "
+                                + "de notas: %.3f - Promedio cualitativo: %s\n",
+                                reporte,
+                                nombresEstudiante[i],
+                                sumaNotas[i],
+                                NotasCualitativas[i]);
+                    }
+
+                    System.out.printf("%s\n", reporte);
     }
 }
+
